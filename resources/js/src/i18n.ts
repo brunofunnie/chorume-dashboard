1477c2fd3bc0f35ba8cf2a01be3a3881ef9a4234
Bruno Oliveira <5279392+brunofunnie@users.noreply.github.com>
2023-12-27 21:39:44 -0300

    Initial commit 2

---
+++ resources/js/src/i18n.ts
@@ -0,0 +1,9 @@
+import { createI18n } from 'vue-i18n';
+
+export default createI18n({
+    legacy: false,
+    allowComposition: true,
+    locale: 'en',
+    globalInjection: true,
+    fallbackLocale: 'en',
+});
